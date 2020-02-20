e51ea9e29bbc57c2014bd315a42968d85d0f15e3
GitCrey <assassincreydt@gmail.com>
2020-02-20 10:47:49 +0100

    19-02-2020
    Min progress efter första lektionen. Skapade en tvådimensionell array, och en for-loop som visade den.

--- Program.cs
+++ Program.cs
@@ -11,6 +11,27 @@ namespace SlutProjektet
         static void Main(string[] args)
         {
 
+            int[,] chessBoard = new int[8, 8]; //kommatecknet avgör hur många dimensioner arrayen har. Ett "," gör det två-dimensionellt. Två ",," gör det tre.
+
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    chessBoard[0, 1] = 1;
+
+                    if (chessBoard[x, y] == 1)
+                    {
+                        Console.WriteLine("Bööö");
+                    }
+
+                    Console.Write(chessBoard[x, y]);
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
