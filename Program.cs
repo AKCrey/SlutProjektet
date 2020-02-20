5f54bf37430ec6c43ef7b9205db6e82593ee2d59
GitCrey <assassincreydt@gmail.com>
2020-02-20 10:44:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlutProjektet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
