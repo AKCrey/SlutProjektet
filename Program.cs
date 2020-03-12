c822b15ff5bc8a72ab1278f271dab24ee7cf420c
GitCrey <assassincreydt@gmail.com>
2020-03-12 11:08:16 +0100

    Returnera flera värden från en metod
    Micke visade mig en extremt hjälpande teknik, som gör att jag nu kan returnera både X och Y värden från min metod. Just nu returnerar jag även location, men jag tror att det skapar ett problem som gör att man fortfarande inte kan flytta runt sig. X och Y kommer jag defenetivt behöva, men jag tror att "int location" i currentState kan tas bort.

--- Program.cs
+++ Program.cs
@@ -13,35 +13,40 @@ namespace SlutProjektet
 
             //Jag har lagt till en slumpgenerator på siffrorna. En idee är att beroende på vilken siffra det är så kommer rummet att vara annorlunda. Jag göra därför massa if-satser så att varje rum man går in i har en chans att bli annorlunda. 
 
+            //Minesweeper
+
             Random generator = new Random();
 
             int[,] chessBoard = new int[8, 8]; //kommatecknet avgör hur många dimensioner arrayen har.
 
             chessBoard = MapKey(chessBoard);
 
-            int location = chessBoard[0, 0];
+            /*int x = 0;
+            int y = 0;*/
+
+            (int x, int y, int location) currentState = (0, 0, 0); //Pröva att ta bort "int location"
+
+            int location = chessBoard[currentState.x, currentState.y];
 
             while (location != 3)
             {
                 if (location == 0)
                 {
-                    Console.WriteLine("DUNGEON MASTER");
+                    Console.WriteLine("0");
+                    currentState = Movement(currentState, chessBoard);
                 }
                 else if (location == 1)
                 {
-                    Console.WriteLine("WÄ");
+                    Console.WriteLine("1");
+                    currentState = Movement(currentState, chessBoard);
                 }
                 else if (location == 2)
                 {
                     Console.WriteLine("2");
+
+                    currentState = Movement(currentState, chessBoard);
                 }
-                Console.ReadLine();
-                Console.Clear();
             }
-
-            string movement = "";
-
-            movement = Movement(movement);
 
             Console.ReadLine();
 
@@ -82,7 +87,7 @@ namespace SlutProjektet
             return chessBoard;
 
         }
-        static string Movement (string movement)
+        static (int x, int y, int location) Movement ((int x, int y, int location) state, int[,] chessBoard)
         {
             Console.WriteLine("Press W for Up");
             Console.WriteLine("Press A for Left");
@@ -93,27 +98,31 @@ namespace SlutProjektet
 
             while (input != "w" && input != "a" && input != "s" && input != "d")
             {
+                Console.WriteLine("Press W for Up");
+                Console.WriteLine("Press A for Left");
+                Console.WriteLine("Press S for Down");
+                Console.WriteLine("Press D for Right");
                 input = Console.ReadLine();
             }
 
             if (input == "w")
             {
-
+                state.location = chessBoard[state.x, state.y - 1];
             }
             else if (input == "a")
             {
-
+                state.location = chessBoard[state.x - 1, state.y];
             }
             else if (input == "s")
             {
-
+                state.location = chessBoard[state.x, state.y + 1];
             }
             else if (input == "d")
             {
-
+                state.location = chessBoard[state.x + 1, state.y];
             }
 
-            return movement;           
+            return state;
         }
     }
 }
