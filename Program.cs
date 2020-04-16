f34d077820dfe6c568d5181c04ac3f2e30742b68
GitCrey <assassincreydt@gmail.com>
2020-04-16 10:02:34 +0200

    Kommentarer och finslipning
    Glömde att committa igår så den här committen är från gårdagens lektion. Jag kommenterade all viktig kod och fixade de sista metoderna så att koden blir lättare läst.

--- Program.cs
+++ Program.cs
@@ -10,66 +10,41 @@ namespace SlutProjektet
     {
         static void Main(string[] args)
         {
-
-            //Jag har lagt till en slumpgenerator på siffrorna. En idee är att beroende på vilken siffra det är så kommer rummet att vara annorlunda. Jag göra därför massa if-satser så att varje rum man går in i har en chans att bli annorlunda. 
-
-            //Minesweeper
-
-            Random generator = new Random();
             
             int[,] chessBoard = new int[8, 8]; //kommatecknet avgör hur många dimensioner arrayen har.
-
-            Console.WriteLine("Press Y to start");
-
-            string goAgane = Console.ReadLine().ToLower().Trim();
-
-            if (goAgane != "y") //Om man bestämmer sig för att inte skriva in Y så får man skylla sig själv
-            {
-                Console.WriteLine("Well then, get lost");
-            }
+            
+            string goAgane = Start();
 
             while (goAgane == "y")
             {
-                int playerHP = 50;
+                int playerHP = 50; //livet anges här nere för att man ska kunna köra om spelet och börja med 50 hp. 
 
-                for (int y = 0; y < 8; y++)
-                {
-                    for (int x = 0; x < 8; x++)
-                    {
-                        chessBoard[x, y] = generator.Next(3);
-                        Console.Write(chessBoard[x, y]);
-                    }
-                    Console.WriteLine();
-                }
+                chessBoard = FirstMap(chessBoard); //Arrayen måste skrivas ut en gång först för att kordinaterna ska anges värden.
 
-                Console.Clear(); //Den måste ha skapats för att jag ska kunna referera till den senare, men eftersom jag vill ha en i loopen så vill jag cleara den här. 
-
-                (int x, int y, int location) currentState = (0, 0, 0); //Pröva att ta bort "int location", den ska ju inte få ett värde i början. 
+                (int x, int y, int location) currentState = (0, 0, 0); //x och y ligger i samma variabel för att man ska returnera båda värderna i 1 metod samtidigt. "location" avgör inte vart man är, utan vilket rum det kommer bli. 
 
                 currentState.location = chessBoard[currentState.x, currentState.y];
 
                 while (currentState.location != 3)
                 {
-                    if (currentState.location != 3 )
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                    }
-                    
+
                     chessBoard = Map(chessBoard); //Denna metod anropas vare gång loopen körs om, vilket är bra då man konstant kan se kartan. 
+
+                    Console.WriteLine("Your location: " + currentState.x + ", " + currentState.y);//Det här är lite fucked då 0,0 är högst upp i vänstra hörnet. 
 
                     if (currentState.location == 0)
                     {
-                        Console.WriteLine("0");
+                        //Console.WriteLine("0");
 
                         playerHP = battle(playerHP);
 
                         if (playerHP <= 0)
                         {
-                            currentState.location = 3;
+                            currentState.location = 3; //Dags att dö
                         }
                         else
                         {
-                            chessBoard = Map(chessBoard);
+                            chessBoard = Map(chessBoard); //Eftersom kartan försvinner under striden vill jag visa den igen efter. 
 
                             currentState = Movement(currentState, chessBoard);
                         }
@@ -77,13 +52,13 @@ namespace SlutProjektet
                     }
                     else if (currentState.location == 1)
                     {
-                        Console.WriteLine("1");
+                        //Console.WriteLine("1");
                         currentState = Movement(currentState, chessBoard);
 
                     }
                     else if (currentState.location == 2)
                     {
-                        Console.WriteLine("2");
+                        //Console.WriteLine("2");
 
                         currentState = Movement(currentState, chessBoard);
 
@@ -92,7 +67,7 @@ namespace SlutProjektet
                     Console.Clear();
                 }
 
-                if (playerHP >= 0)
+                if (playerHP <= 0)
                 {
                     Console.WriteLine("GAME OVER");
                     Console.WriteLine("");
@@ -106,26 +81,39 @@ namespace SlutProjektet
             Console.ReadLine();
 
         }
-        /*static int[,] MapKey (int[,] chessBoard) //Metoden för klicka på 1 för att anropa Map metoden
+        static string Start ()
         {
-            Console.WriteLine("Press 1 for the map");
+            Console.WriteLine("Press Y to start");
 
-            string input = Console.ReadLine();
+            string goAgane = Console.ReadLine().ToLower().Trim();
 
-            int mapKey = 0;
-
-            bool success = int.TryParse(input, out mapKey);
-
-            if (mapKey == 1)
+            if (goAgane != "y") //Om man bestämmer sig för att inte skriva in Y så får man skylla sig själv
             {
-                chessBoard = Map(chessBoard);
+                Console.WriteLine("Well then, get lost");
             }
 
-            return chessBoard;
-        }Just nu har jag inget behov av att detta. Kartan ska istället visas hela tiden*/
-        static int[,] Map (int[,] chessBoard) //Visar kartan.
+            return goAgane;
+        }
+        static int[,] FirstMap (int[,] chessBoard) //Metoden för klicka på 1 för att anropa Map metoden
         {
             Random generator = new Random();
+
+            for (int y = 0; y < 8; y++) 
+            {
+                for (int x = 0; x < 8; x++) //Denna loop gör att för varje gång y går upp med 1 så körs den andra som skriver ut 8 värden på rad. 
+                {
+                    chessBoard[x, y] = generator.Next(3); //Denna rad slumpar in ett tal mellan 0-2 i varje kordinat som skapats. 
+                    Console.Write(chessBoard[x, y]);
+                }
+                Console.WriteLine();//Den byter rad.
+            }
+
+            Console.Clear(); //Arrayen måste ha skapats för att jag ska kunna referera till den senare, men eftersom jag vill ha en i loopen så vill jag cleara den här. 
+
+            return chessBoard;
+        }
+        static int[,] Map (int[,] chessBoard) //Visar kartan.
+        {
 
             for (int y = 0; y < 8; y++)
             {
@@ -141,54 +129,11 @@ namespace SlutProjektet
         }
         static (int x, int y, int location) Movement ((int x, int y, int location) state, int[,] chessBoard)
         {
-            if (state.y > 0)
-            {
-                Console.WriteLine("Press W for Up");
-            }
-            
-            if (state.x > 0)
-            {
-                Console.WriteLine("Press A for Left");
-            }
-            
-            if (state.y < 7)
-            {
-                Console.WriteLine("Press S for Down");
-            }
-            
-            if (state.x < 7)
-            {
-                Console.WriteLine("Press D for Right");
-            }
+            string input = MovementControl(state.x, state.y); //Metoden hämtar in vilken knapp som användaren tryckt för att förflytta sig. 
 
-            string input = Console.ReadLine().ToLower().Trim();
+            input = MovementControlCheck(state.x, state.y, input); //Om användaren inte skrev in något av rörelse-alternativen.
 
-            while (input != "w" && input != "a" && input != "s" && input != "d")
-            {
-                if (state.y > 0)
-                {
-                    Console.WriteLine("Press W for Up");
-                }
-
-                if (state.x > 0)
-                {
-                    Console.WriteLine("Press A for Left");
-                }
-
-                if (state.y < 7)
-                {
-                    Console.WriteLine("Press S for Down");
-                }
-
-                if (state.x < 7)
-                {
-                    Console.WriteLine("Press D for Right");
-                }
-                
-                input = Console.ReadLine().ToLower().Trim();
-            }
-
-            if (input == "w" && state.y > 0)
+            if (input == "w" && state.y > 0) //state.y > 0 säkerhetskollar så att man inte kan gå uppåt även om man inte fått det alternativet, så att spelet inte kraschar. 
             {
                 state.y = state.y - 1;
 
@@ -208,92 +153,179 @@ namespace SlutProjektet
 
             state.location = chessBoard[state.x, state.y]; //Uppdaterar locationen utifrån vad användaren har klickat på för knapp.
 
-            return (state.x, state.y, state.location);
+            return (state.x, state.y, state.location); //man har redan flyttat på sig men värdena måste behållas för nästa förflyttning.
+        }
+        static string MovementControl(int x, int y)
+        {
+            if (y > 0) //Om man står på kanten ska man inte ens få alternativet att gå in i väggen. 
+            {
+                Console.WriteLine("Press W for Up");
+            }
+
+            if (x > 0)
+            {
+                Console.WriteLine("Press A for Left");
+            }
+
+            if (y < 7)
+            {
+                Console.WriteLine("Press S for Down");
+            }
+
+            if (x < 7)
+            {
+                Console.WriteLine("Press D for Right");
+            }
+
+            string input = Console.ReadLine().ToLower().Trim();
+
+            return input;
+        }
+        static string MovementControlCheck (int x, int y, string input)
+        {
+
+            while (input != "w" && input != "a" && input != "s" && input != "d") //Den klassiska while-loopen som kontrollerar att användaren skriver in rätt alternativ. 
+            {
+                if (y > 0)
+                {
+                    Console.WriteLine("Press W for Up");
+                }
+
+                if (x > 0)
+                {
+                    Console.WriteLine("Press A for Left");
+                }
+
+                if (y < 7)
+                {
+                    Console.WriteLine("Press S for Down");
+                }
+
+                if (x < 7)
+                {
+                    Console.WriteLine("Press D for Right");
+                }
+
+                input = Console.ReadLine().ToLower().Trim();
+            }
+
+            return input;
         }
         static int battle(int playerHP) //Här är min strid lagrad i en metod
         {
             Random generator = new Random();
 
-            string fighterA = "You";
+            string fighterA = "You"; //För framtida ändringar och tydligare kod gjorde jag även en fighterA istället för att bara skriva "You"
 
             string fighterB = "Dungeon Master";
 
-            int hpA = playerHP;
-            int hpB = 20;
+            int enemyHP = 20; //Motståndarens HP. 
 
-            Console.WriteLine("A " + fighterB + " attacks!");
-            Console.WriteLine("");
+            EnemySpotted(fighterB);
 
-            while (hpA > 0 && hpB > 0) //Så länge båda karaktärer har över 0 hp så fortsätter striden//
+            while (playerHP > 0 && enemyHP > 0) //Så länge båda karaktärer har över 0 hp så fortsätter striden
             {
-                Console.WriteLine("Your HP: " + hpA);
-                Console.WriteLine(fighterB + " HP: " + hpB);
+                string choiceAttack = AttackChoice(fighterB, playerHP, enemyHP); //Anropar attack-valet för SPELAREN
 
-                Console.WriteLine("");
+                enemyHP = UserAttack(choiceAttack, enemyHP, fighterA, fighterB); //Anropar attack-metoden för SPELAREN
 
-                Console.WriteLine("Choose an Attack! (Write the number)");
-                Console.WriteLine("1. Lightsaber Strike (Low DMG but high chance of hitting)");
-                Console.WriteLine("2. Force Push (High DMG but low chance of hitting)");
-
-                string choiceAttack = Console.ReadLine().Trim();
-
-                while (choiceAttack != "1" && choiceAttack != "2" && choiceAttack != "1." && choiceAttack != "2.") //Om man inte skriver in attack-nummret så loopas det.
-                {
-                    Console.WriteLine("Wrong Input");
-                    choiceAttack = Console.ReadLine();
-                }
-
-                Console.WriteLine("");
-
-                int dmgLightA = generator.Next(7, 9); //Random DMG för både light och heavy. Detta för att göra det lite mer varierat.
-                int dmgHeavyA = generator.Next(12, 16);
-
-                if (choiceAttack == "1" || choiceAttack == "1.") //När man väljer attack 1 (light attack) så händer följande:
-                {
-                    hpB = hpB - dmgLightA;
-                    Console.WriteLine(fighterA + " did " + dmgLightA + " DMG to " + fighterB); //Vill skriva ut hur mycket dmg man gör mot motståndaren
-                }
-                else if (choiceAttack == "2" || choiceAttack == "2.")//När man väljer attack 2 (heavy) så händer följande:
-                { 
-                    hpB = hpB - dmgHeavyA;
-                    Console.WriteLine(fighterA + " did " + dmgHeavyA + " DMG to " + fighterB); //Fungerar på samma sätt som light attacken
-                }
-
-                Console.WriteLine("");
-
-                int trooperAttack = generator.Next(1, 3); // 50/50 om det är light eller heavy attack
-                int dmgLightShadow = generator.Next(5, 7);
-                int dmgHeavyShadow = generator.Next(8, 11);
-
-                if (trooperAttack == 1)
-                {
-                    hpA = hpA - dmgLightShadow;
-                    Console.WriteLine(fighterB + " used: Light attack!");
-                    Console.WriteLine(fighterB + " did " + dmgLightShadow + " DMG");
-                }
-                else if (trooperAttack == 2)
-                {
-                    hpA = hpA - dmgHeavyShadow;
-                    Console.WriteLine(fighterB + " used: Heavy attack!");
-                    Console.WriteLine(fighterB + " did " + dmgHeavyShadow + " DMG to you");
-                }
-
-                Console.ReadLine();
+                playerHP = EnemyAttack(playerHP, fighterB); //Anropar metoden för MOTSTÅNDAREN
 
                 Console.Clear();//Eftersom det är en del text och val man måste göra varje gång, så ville jag Cleara det efter varje runda. 
             }
 
-            if (hpB <= 0) //Om motståndaren har mindre eller lika med 0 så vinner spelaren! Yay :D
+            PlayerWin(enemyHP, fighterB);
+
+            return playerHP; //playerHP måste komma till baka till main-koden
+        }
+        static void EnemySpotted(string fighterB) //Eftersom jag bara har text behöver jag inte returna något värde. 
+        {
+            Console.WriteLine("");
+            Console.WriteLine("A " + fighterB + " attacks!");
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to continue");
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+        static string AttackChoice (string fighterB, int playerHP, int enemyHP) //Metoden för användarens attack choice. 
+        {
+            Console.WriteLine("Your HP: " + playerHP);
+            Console.WriteLine(fighterB + " HP: " + enemyHP);
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Choose an Attack! (Write the number)");
+            Console.WriteLine("1. Light Attack (7-9 DMG)");
+            Console.WriteLine("2. Heavy Attack (12-16 DMG)");
+
+            string choiceAttack = Console.ReadLine().Trim();
+
+            while (choiceAttack != "1" && choiceAttack != "2" && choiceAttack != "1." && choiceAttack != "2.") //Om man inte skriver in attack-nummret så loopas det.
+            {
+                Console.WriteLine("Wrong Input");
+                choiceAttack = Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+
+            return choiceAttack;
+        }
+        static int UserAttack (string choiceAttack, int enemyHP, string fighterA, string fighterB) //Metoden för spelarens attack
+        {
+            Random generator = new Random();
+
+            int dmgLightA = generator.Next(7, 9); //Random DMG för både light och heavy. Detta för att göra det lite mer varierat.
+            int dmgHeavyA = generator.Next(12, 16);
+
+            if (choiceAttack == "1" || choiceAttack == "1.") //När man väljer attack 1 (light attack) så händer följande:
+            {
+                enemyHP = enemyHP - dmgLightA;
+                Console.WriteLine(fighterA + " did " + dmgLightA + " DMG to " + fighterB); //Vill skriva ut hur mycket dmg man gör mot motståndaren
+            }
+            else if (choiceAttack == "2" || choiceAttack == "2.")//När man väljer attack 2 (heavy) så händer följande:
+            {
+                enemyHP = enemyHP - dmgHeavyA;
+                Console.WriteLine(fighterA + " did " + dmgHeavyA + " DMG to " + fighterB); //Fungerar på samma sätt som light attacken
+            }
+
+            Console.WriteLine("");
+
+            return enemyHP;
+        }
+        static int EnemyAttack (int playerHP, string fighterB) //Metoden för motståndarens attack. 
+        {
+            Random generator = new Random();
+
+            int attack = generator.Next(1, 3); // 50/50 om det är light eller heavy attack
+            int dmgLight = generator.Next(5, 7);
+            int dmgHeavy = generator.Next(8, 11);
+
+            if (attack == 1) //Om attack blir 1, gör motståndaren en light attack på dmgLight DMG. 
+            {
+                playerHP = playerHP - dmgLight;
+                Console.WriteLine(fighterB + " used: Light attack!");
+                Console.WriteLine(fighterB + " did " + dmgLight + " DMG");
+            }
+            else if (attack == 2) //Osv med attack 2. 
+            {
+                playerHP = playerHP - dmgHeavy;
+                Console.WriteLine(fighterB + " used: Heavy attack!");
+                Console.WriteLine(fighterB + " did " + dmgHeavy + " DMG to you");
+            }
+
+            Console.ReadLine();
+
+            return playerHP; //Eftersom det är spelarens HP som motståndaren slagit påp måste spelarens HP returneras. 
+        }
+        static void PlayerWin (int enemyHP, string fighterB) //Eftersom jag bara har text behöver jag inte returna något värde. 
+        {
+            if (enemyHP <= 0) //Om MOTSTÅNDAREN har mindre eller lika med 0 HP så vinner spelaren
             {
                 Console.WriteLine(fighterB + " died");
                 Console.WriteLine("Fight won!");
             }
 
-            playerHP = hpA; //hpA skrivs över till playerHP
-
             Console.ReadLine();
-
-            return playerHP; //playerHP måste komma till baka till main-koden
         }
     }
 }
